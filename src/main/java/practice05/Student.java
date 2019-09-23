62126741ae38896a4ec39804c00491eb34e0f25c
Lester Jan Artienda <artiele@oocl.com>
2019-09-23 17:13:59 +0800

    Passed practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce() {
+        return super.introduce().concat(" I am a Student. I am at Class "+getKlass()+".");
+    }
 }
