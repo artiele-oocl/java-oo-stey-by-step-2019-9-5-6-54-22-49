ce146121cf16e4c9e8b9e26041e70aacb5e37740
Lester Jan Artienda <artiele@oocl.com>
2019-09-23 18:14:51 +0800

    Passed practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce() {
+        String withKlass = super.introduce().concat(" I am a Teacher. I teach Class "+getKlass()+".");
+        String withoutKlass = super.introduce().concat(" I am a Teacher. I teach No Class.");
+        return getKlass() != 0 ? withKlass : withoutKlass;
+    }
 }
