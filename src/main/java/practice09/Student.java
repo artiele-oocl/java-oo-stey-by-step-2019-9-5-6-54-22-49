50151d6b4bee4ddea01e79107964666597760df6
Lester Jan Artienda <artiele@oocl.com>
2019-09-24 16:52:27 +0800

    Passed practice09

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,20 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce() {
+        if (getKlass() != null && klass.getLeader() != null && klass.getLeader().getName().equals(this.getName())) {
+            return super.introduce().concat(" I am a Student. I am Leader of "+klass.getDisplayName()+".");
+        } else {
+            return super.introduce().concat(" I am a Student. I am at "+klass.getDisplayName()+".");
+        }
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    private String subIntro = " I am a Teacher. I teach ";
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        if (getKlass() != null) return super.introduce().concat(subIntro+klass.getDisplayName()+".");
+        else return super.introduce().concat(subIntro+"No Class.");
+    }
+    public String introduceWith(Student student) {
+        return isMyStudent(student) ? super.introduce().concat(subIntro+student.getName()+".") : super.introduce().concat(" I am a Teacher. I don't teach "+student.getName()+".");
+    }
+    private boolean isMyStudent(Student student) {
+        return klass.getNumber() == student.getKlass().getNumber();
+    }
 }
