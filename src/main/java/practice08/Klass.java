32a0d50c303848f2663a2f9bde5d884a5486021e
Lester Jan Artienda <artiele@oocl.com>
2019-09-24 15:08:24 +0800

    Passed practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,22 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+    public Student getLeader() {
+        return leader;
+    }
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName() {
+        return "Class "+getNumber();
+    }
 }
